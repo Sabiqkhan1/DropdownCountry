bf454b74a4a868090b56a846ba3e45572dbd619d
Sabiqkhan1 <52681305+Sabiqkhan1@users.noreply.github.com>
2021-04-15 06:20:47 +0100

    Update Repository.js

--- CountryDropdown/src/api/Repository.js
+++ CountryDropdown/src/api/Repository.js
@@ -1,7 +1,7 @@
 import { api } from './BaseURL';
 
 
-// getCountries function this function will be valled when a region dropdown is chaanged and on change 
+// getCountries function this function will be called when a region dropdown is changed (on change)
 // The value of the elected dropdown will be passed as parameters as data. (which will be europe or asia)
 const getCountries = async (data) => {
     //concatinate the data with the Api
